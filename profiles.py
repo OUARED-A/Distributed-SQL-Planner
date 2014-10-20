ff1e990a4bb82d45c117d79f8b3ece1dc7ce2750
Enrico Bacis <enrico.bacis@gmail.com>
2014-10-20 18:35:10 +0200

    Use new semantic

--- profiles.py
+++ profiles.py
@@ -46,28 +46,24 @@ def selection(node, inputs):
     pl = inputs[0].profile
 
     for columns in _get_column_groups_from(node):
-        if len(columns) > 1:           # update equivalence class
-            pl = pl._replace(sim=deepcopy(pl.sim).add(*columns))
-        if columns <= pl.e:            # both encrypted
-            pl = pl._replace(ie=pl.ie | columns)
-        elif columns <= pl.v | pl.e:   # if mixed require in iv
+        if not columns < pl.v | pl.e:
+            raise ValueError('some columns %s not in children' % columns)
+        if len(columns) > 1:          # update equivalence class
+            pl = pl._replace(sim=eqclass(columns).merge(pl.sim))
+        elif columns <= pl.v:         # plaintext
             pl = pl._replace(iv=pl.iv | columns)
-        else:
-            raise ValueError('selected columns %s not in children' % columns)
+        else:                         # encrypted
+            assert columns <= pl.e
+            pl = pl._replace(ie=pl.ie | columns)
     return pl
 
 
 def join(node, inputs):
     assert len(inputs) == 2
     pl, pr = (input.profile for input in inputs)
-    groups = _get_column_groups_from(node)
-    columns = set.union(*groups) if groups else set()
-    sim = reduce(lambda sim, columns: sim.add(*columns), groups,
-                 deepcopy(pl.sim).merge(pr.sim))
-    profile = Profile(v=pl.v | pr.v, e=pl.e | pr.e, iv=pl.iv | pr.iv,
-                      ie=pl.ie | pr.ie | columns, sim=sim)
-    return (profile if columns <= profile.v else
-            profile._replace(v=profile.v - columns, e=profile.e | columns))
+    sim = eqclass(_get_column_groups_from(node)).merge(pl.sim).merge(pr.sim)
+    return Profile(v=pl.v | pr.v, e=pl.e | pr.e, iv=pl.iv | pr.iv,
+                   ie=pl.ie | pr.ie, sim=sim)
 
 
 def aggregate(node, inputs):
